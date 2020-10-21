d9e25151497571126852aee42f8d70eead3182e1
Mauro.jr <mauro.jr@hotmail.fr>
2020-10-21 14:29:28 -0300

    Primeiro commit, estrutura do projeto.

---
+++ KG_Travel/KG_Travel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KG_Travel
+//
+//  Created by Mauro Siqueira Jr on 21/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
